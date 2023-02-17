c63b5b22a8a784621c814ce7853795506af56038
Braimah Suadik <abdulrashidsuadik@gmail.com>
2023-02-16 16:35:39 -0800

    write a c program that prints a text and prints to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - main function
+ *
+ * Return: return 1 function
+ */
+int main(void)
+{
+    const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    for (int i = 0; msg[i] != '\0'; i++) {
+        putchar(msg[i]);
+    }
+    return 1;
+}
+
