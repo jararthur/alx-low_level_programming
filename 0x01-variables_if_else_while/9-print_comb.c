326fd84551fa29faf1f6e8ef9a2e0619ba8354db
Braimah Suadik <abdulrashidsuadik@gmail.com>
2023-02-18 14:13:59 -0800

    printing numbers and comma with space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - this is the function called main
+ *
+ * Return: this function returns o at the end of running the code
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
