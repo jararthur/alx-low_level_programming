9356faa5d3b2e943acc1dd3ce93070e0cacfcd7e
Braimah Suadik <abdulrashidsuadik@gmail.com>
2023-02-18 10:18:13 -0800

    printing 1 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - this is where the code starts execution
+ *
+ * Return: This code should return o after execution
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d\n", n);
+	}
+	return (0);
+}
