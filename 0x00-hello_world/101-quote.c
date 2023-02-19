cbef3af27dfa22354ac398925a13c8dfbdb51c7f
Braimah Suadik <abdulrashidsuadik@gmail.com>
2023-02-19 07:09:44 -0800

    its not gonna check

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,10 +6,9 @@
  */
 int main(void)
 {
-    const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    for (int i = 0; msg[i] != '\0'; i++) {
-        putchar(msg[i]);
-    }
-    return 1;
+	const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	for (int i = 0; msg[i] != '\0'; i++) 
+	{putchar(msg[i]);}
+	return (1);
 }
 
