cc4c13a801062f8c2ea220bc999ea5788897db8a
Braimah Suadik <abdulrashidsuadik@gmail.com>
2023-02-26 16:23:46 +0000

    natural numbers below 10 that are multiples of 3 or 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Return: 0 (success) 
+ * Return: 0 (success)
  */
 int main(void)
 {
 	int i = 0;
 	long j = 1, k = 2;
+
 	while (i < 50)
 	{
 		if (i == 0)
 			printf("%ld", j);
 		else if	(i == 1)
 			printf(", %ld", k);
-		else 
+		else
 		{
 			k += j;
 			j = k - j;
