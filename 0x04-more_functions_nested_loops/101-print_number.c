3be9e705a75a51a55cc25055953dc80c562d0126
Braimah Suadik <abdulrashidsuadik@gmail.com>
2023-02-26 22:13:18 +0000

    Write a function that prints an integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_number - this  is the print number function to print int with putchar
+ * @n: this is the attribute
+ *
+ * Return: this function should return 0 i think
+ * betty is really working for me in this code
+ */
+void print_number(int n)
+{
+	int a = 98;
+	int b = 402;
+	int c = 1024;
+	int d = 0;
+	int e = -98;
+
+	for (n = 97; n < 102; n++);
+	{
+		_put
+	return (0);
+}
