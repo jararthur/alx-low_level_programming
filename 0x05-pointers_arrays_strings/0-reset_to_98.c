c4b203b7cfae6399769b42b21c5cbe2b5e77f500
Braimah Suadik <abdulrashidsuadik@gmail.com>
2023-02-28 22:24:31 +0000

    Write a function that takes a pointer to an int as parameter and updates the value it points to to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - this is the main file
+ * @n: this take an attribute to memory place n
+ *
+ * Return: this function returns void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+This is a readme file for this directory
+it specifies all the 
