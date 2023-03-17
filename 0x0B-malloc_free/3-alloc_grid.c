3bff7c2012dd7c8e514b756bf174d55a7ca899d5
Suadik Braimah <abdulrashidsuadik@gmail.com>
2023-03-17 17:41:39 +0000

    3 corrections

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,9 +13,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-
 	mee = malloc(sizeof(int *) * height);
-
 	if (mee == NULL)
 		return (NULL);
 	for (x = 0; x < height; x++)
@@ -25,16 +23,14 @@ int **alloc_grid(int width, int height)
 		{
 			for (; x >= 0; x--)
 				free(mee[x];
-
-			free(mee);
-			return (NULL);
-		}
-	}
+						free(mee);
+						return (NULL);
+						}
+						}
 	for (x = 0; x < height; x++)
 	{
 	for (y = 0; y < width; y++)
 	mee[x][y] = 0;
 	}
-
 	return (mee);
-}
+	}
