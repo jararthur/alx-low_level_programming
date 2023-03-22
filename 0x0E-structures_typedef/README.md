599fd05786e8d9f599e1ea3038f771707715f350
Suadik Braimah <abdulrashidsuadik@gmail.com>
2023-03-22 05:39:08 +0000

    desfine a new type struct dog with the ff element

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,4 @@
+this is a readme for this repo
+it contains nothing but description of structures
+third line
+very good
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,36 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+/**
+ * struct dog - a dog's basic info
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the dog's owner's name
+ *
+ * This structure represents basic information about a dog, including its name,
+ * age, and owner's name.
+ */
+	struct dog
+	{
+		char *name;
+		float age;
+		char *owner;
+	};
+
+
+	/**
+	* dog_t - typedef for struct dog
+	*/
+	typedef struct dog dog_t;
+
+
+	void init_dog(struct dog *d, char *name, float age, char *owner);
+	void print_dog(struct dog *d);
+	dog_t *new_dog(char *name, float age, char *owner);
+	void free_dog(dog_t *d);
+	char *_strcpy(char *dest, char *src);
+	int _strlen(char *s);
+
+
+	#endif
